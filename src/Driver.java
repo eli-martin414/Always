fb7eafd12cfe37437c3f5de4c1b249bedca65ce6
Eli Martin <elizabeth.k.martin-1@ou.edu>
2020-11-02 17:00:53 -0600

    added peek() and default no args constructor

--- src/Driver.java
+++ src/Driver.java
@@ -1,5 +1,12 @@
 
 public class Driver
 {
-
+		public static void main(String[] args)
+		{
+			System.out.println("GitHub!!!");
+			System.out.println("Eclipse!!!");
+			SuperStack<String> stack = new SuperStack<>();
+			stack.push("aha");
+			System.out.println(stack.peek());
+		}
 }
